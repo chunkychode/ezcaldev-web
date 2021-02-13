054f162b376f33f9e2d39fb9e72233d45f4f1218
allenw23 <allenw23@gmail.com>
2021-02-13 13:25:32 -0800

    call chunkycal auth

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,12 +4,30 @@ import withSession from "../../lib/session";
 const VALID_EMAIL = "test@test.com";
 const VALID_PASSWORD = "test";
 
+const authenticateUser = async ({ email, password }) => {
+  try {
+    const url = process.env.AUTH_URL;
+    const body = { email, password };
+    const post = {
+      method: "post",
+      body: JSON.stringify(body),
+    };
+    const res = await fetchJson(url, post);
+    return res;
+  } catch (e) {
+    throw e;
+  }
+};
+
 export default withSession(async (req, res) => {
   try {
     if (req.method === "POST") {
       const { email, password } = req.body;
 
-      if (email === VALID_EMAIL && password === VALID_PASSWORD) {
+      const res = await authenticateUser({ email, password });
+      console.log("res", res);
+
+      if (res?.message !== "Invalid credentials") {
         const user = { isLoggedIn: true, email };
         req.session.set("user", user);
         await req.session.save();
