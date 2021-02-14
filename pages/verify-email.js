d53d34b497bb4b5495a17fedfa9a5087f9efd99f
allenw23 <allenw23@gmail.com>
2021-02-14 10:41:31 -0800

    add create owner, claims to verify email

--- pages/verify-email.js
+++ pages/verify-email.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
+import { message } from "statuses";
 import Layout from "../components/Layout";
-import { verifyEmail as _verifyEmail } from "../lib/auth";
+import { authenticateUser, verifyEmail as _verifyEmail } from "../lib/auth";
+import { createCalOwner, createClaimsForCal } from "../lib/calOnboarding";
 
 export default function verifyEmail({ data }) {
   return (
@@ -19,16 +21,47 @@ export default function verifyEmail({ data }) {
 }
 
 export async function getServerSideProps({ query }) {
-  let data = {};
-  const emailVerificationToken = query.v;
-  if (emailVerificationToken) {
-    const res = await _verifyEmail({ emailVerificationToken });
-    console.log("verifyEmail", res);
-    if (res.message === "updated") {
-      data.emailVerified = true;
-    }
-  }
+  try {
+    let data = {};
+    const emailVerificationToken = query.v;
+    if (emailVerificationToken) {
+      // grab the app auth token for subsequent calls
+      const AUTH_UID = process.env.AUTH_UID;
+      const AUTH_PWD = process.env.AUTH_PWD;
 
-  // Pass data to the page via props
-  return { props: { data } };
+      const authResp = await authenticateUser({
+        email: AUTH_UID,
+        password: AUTH_PWD,
+      });
+      const { authToken } = authResp;
+
+      const res = await _verifyEmail({ authToken, emailVerificationToken });
+      if (res.message === "updated") {
+        data.emailVerified = true;
+      }
+
+      // create owner
+      const createOwnerRes = await createCalOwner({ authToken });
+      const { ownerId } = createOwnerRes;
+
+      if (!ownerId) {
+        throw new Error("Unable to create calendar owner");
+      }
+      // create claims
+      const createClaimsRes = await createClaimsForCal({
+        // todo: API requires this field but will use the valid token correctly
+        email: "nobody@nowhere.com",
+        emailVerificationToken,
+        ownerId,
+        authToken,
+      });
+
+      if (createClaimsRes.message !== "Created") {
+        throw new Error("Unable to create claims");
+      }
+    }
+    return { props: { data } };
+  } catch (e) {
+    console.log("verifyEmail e", e);
+  }
 }
